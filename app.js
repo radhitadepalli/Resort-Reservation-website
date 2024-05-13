534a107002c58b50f992e19258293653f4081fc6
RADHI T V J <radhitadepalli13@gmail.com>
2024-05-13 21:31:19 +0530

    Add project Files ... changed home route

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ const sessionOptions={
         httpOnly:true,
     },
 }
-// app.get("/", (req, res) => {
-//     res.send("Hi, I am root");
-// });
+app.get("/", (req, res) => {
+    res.redirect("/listings")
+});
 
 
 
